6ef6797e11c3514e9ab492ce53713319eeae28b4
dadi11 <dadi.sikorski@gmail.com>
2017-09-10 18:17:05 +0200

    temporary remove xenonhd things
    Change-Id: Ie6eb2d9fa0ea0036483ba0574bccd666553499a9

--- xenonhd.mk
+++ xenonhd.mk
@@ -26,13 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=dadi11
 
-ROOT_METHOD=magisk
-
-PRODUCT_PACKAGES += \
-    Adaway \
-    KernelAdiutor \
-    MiXplorer
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="xiaomi/mido/mido:6.0.1/MMB29M/V8.2.4.0.MCFMIDL:user/release-keys" \
     PRIVATE_BUILD_DESC="mido-user 6.0.1 MMB29M V8.2.4.0.MCFMIDL release-keys"
